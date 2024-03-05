982eecb0363207913a369ac2fd01b0eb07bfe2cc
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-05 12:22:21 +0100

    Modified main in order to reflect changes in directories

--- main.py
+++ main.py
@@ -4,84 +4,8 @@ Created on Sun Jan 21 16:18:27 2024
 
 @author: fbarb
 """
+import functions.LAMMPSreader as reader
 
-def convert_value(value):
-
-    value = value.strip()
-
-    if value.isdigit(): #Convert to int
-        return int(value)
-
-    if '.' in value: #Convert to float
-        try:
-            return float(value)
-        except ValueError:
-            pass
-
-    if value.startswith('[') and value.endswith(']'): #Convert to list
-        elements = value[1:-1].split(',')
-        elements = [elem.strip() for elem in elements]
-        converted_elements = []
-        for elem in elements:
-            if elem.isdigit(): #Convert list element to int
-                converted_elements.append(int(elem))
-            elif '.' in elem: #Convert list element to float
-                try:
-                    converted_elements.append(float(elem))
-                except ValueError:
-                    converted_elements.append(elem)
-            elif not elem:
-                continue
-            else:
-                converted_elements.append(elem)
-
-        return converted_elements #Return list
-
-    return value #Return int, float or string
-
-def read_yaml(file):
-    step_reading = False
-
-    for line in file:
-        if line.startswith('---'):
-            step_reading = True
-
-        while step_reading:
-            if ':' in line and '-' not in line:
-                key = line.split(':')[0].strip()
-                value = line.split(':')[1].strip()
-                if value != '':
-                    value = convert_value(value)
-                    step[key] = value
-                    line = file.readline()
-
-                else:
-                    line = file.readline()
-                    data_reading = True
-                    data_list = []
-                    data_dic = {}
-                    while data_reading:
-                        if '-' in line and ':' not in line:
-                            d_value = line.split('- ')[1]
-                            d_value = convert_value(d_value)
-                            data_list.append(d_value)
-                            step[key] = data_list
-                            line = file.readline()
-                        elif '-' in line and ':' in line:
-                            d_key = line.replace(' ', '').replace('-', '').split(':')[0]
-                            d_value = line.replace(' ', '').replace('-', '').split(':')[1].strip()
-                            d_value = convert_value(d_value)
-                            data_dic[d_key] = d_value
-                            step[key] = data_dic
-                            line = file.readline()
-                        else:
-                            data_reading = False
-
-            elif line.startswith('...'):
-                step_reading = False
-
-            else:
-                break
 
 file_path = './examples/input_example.yaml'
 output = 'dump_file.xyz'
@@ -89,5 +13,5 @@ step = {}
 
 if __name__ == "__main__":
     with open(file_path, 'r') as file:
-        read_yaml(file)
-
+        reader.read_yaml(file, step)
+        

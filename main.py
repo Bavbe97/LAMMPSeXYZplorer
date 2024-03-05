f829e3198722ecc683d72d695ba49021744e6f2a
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-05 12:28:59 +0100

    Renaming to reflect project structure

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Created on Sun Jan 21 16:18:27 2024
 
 @author: fbarb
 """
-import functions.LAMMPSreader as reader
+from LAMMPShade import LAMMPS_reader
 
 
 file_path = './examples/input_example.yaml'
@@ -13,5 +13,5 @@ step = {}
 
 if __name__ == "__main__":
     with open(file_path, 'r') as file:
-        reader.read_yaml(file, step)
+        LAMMPS_reader.read_yaml(file, step)
         
--- tests/LAMMPS_reader_tests.py
+++ tests/LAMMPS_reader_tests.py
@@ -6,8 +6,8 @@ Created on Wed Jan 31 13:10:30 2024
 """
 
 
-import main
+from LAMMPShade.LAMMPS_reader import convert_value as convert_value
 
 if __name__ == "__main__":
-    prova = main.convert_value('55')
+    prova = convert_value('55')
     print (prova)

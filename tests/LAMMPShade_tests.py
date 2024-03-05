43aed67eb903a247c824917287770bd75406f57e
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-05 12:07:52 +0100

    Added tests directory

---
+++ tests/LAMMPShade_tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 13:10:30 2024
+
+@author: fbarb
+"""
+
+
+import main
+
+if __name__ == "__main__":
+    prova = main.convert_value('55')
+    print (prova)

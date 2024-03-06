107026a2b3a0edae273e9a1a9baff03baac03548
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-06 17:58:31 +0100

    Added Graphs class structure (still not working)

---
+++ lammpshade/GraphMaker.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 17:57:05 2024
+
+@author: fbarb
+"""
+
+class GraphMaker:
+    def __init__(self, graph_specifications):
+        self.graph_specifications = graph_specifications
+
+    def make_graph(self, data):
+
+        pass

e53bd51e6eab02e4dd7cc635368b297e7cf405cb
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-06 17:43:48 +0100

    main modified to reflect changes

--- main.py
+++ main.py
@@ -7,12 +7,5 @@ Created on Sun Jan 21 16:18:27 2024
 import pandas as pd
 import lammpshade as ls
 
-
-file_path = './examples/input_example.yaml'
-output = 'dump_file.xyz'
-step = {}
-
-if __name__ == "__main__":
-    with open(file_path, 'r') as file:
-        ls.read_yaml(file, step)
-        
+prova = ls.Simulation('./examples/input_example.yaml')
+thermo_data = prova.get_thermodata()

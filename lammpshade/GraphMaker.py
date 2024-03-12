29d1728b5f82f0fbfd7494e4e61af2376f4f54f8
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-12 17:20:40 +0100

    GraphMaker class is now working as intended

--- lammpshade/GraphMaker.py
+++ lammpshade/GraphMaker.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from lammpshade.Constructor import *
 
+
 class GraphMaker:
     def __init__(self, thermo_data, thermo_keywords, YAMLReader):
         self.thermo_data = thermo_data
@@ -16,7 +17,8 @@ class GraphMaker:
         pass
 
     def make_graph(self):
-        thermo_df = pd.DataFrame(self.thermo_data, columns = self.thermo_keywords)
+        thermo_df = pd.DataFrame(self.thermo_data,
+                                 columns=self.thermo_keywords)
         #color = []
         #markerstyle = []
         #linestyle = []
@@ -24,38 +26,39 @@ class GraphMaker:
         #grid = False
         gridstyle = '--'
         xlabel = 'Time ' + self.file.get_units('Time')
-        #ciclo while per scegliere colonne da plottare + in che modo
         for column in thermo_df:
             if column not in ('Time', 'Step'):
                 fig, ax = plt.subplots()
                 ax.plot(thermo_df['Time'], thermo_df[column])
                 ax.set_xlabel(xlabel)
-                y = column.replace('c_','').replace('v_','')
+                y = column.replace('c_', '').replace('v_', '')
                 ylabel = y + ' ' + self.file.get_units(y.split('_')[0])
                 ax.set_ylabel(ylabel)
                 ax.grid(linestyle=gridstyle)
                 #plt show per salvare eventualmente il file
         plt.show()
-        
+
     def interact_graph(self):
         print('Define graphs printing settings:')
-        print('The following quantities have been found: ' + str(self.thermo_keywords)[1:-1])
+        print('The following quantities have been found: ' +
+              str(self.thermo_keywords)[1:-1])
         while True:
             keyword_check = True
-            print('Input format: mode [q_name1, q_name2, q_name3] \n'+
+            print('Input format: mode [q_name1, q_name2, q_name3] \n' +
                   'To exit the program type: "exit"')
             graph = input('Select which quantities to display and how:\n')
-            prova = graph.replace(' ','').split('[')
+            prova = graph.replace(' ', '').split('[')
             if prova[0] == 'display':
-                keywords_list = prova[1].replace(']','').split(',')
+                keywords_list = prova[1].replace(']', '').split(',')
                 for keyword in keywords_list:
                     if keyword not in self.thermo_keywords:
                         print('Error: ' + keyword + ' not found')
                         keyword_check = False
-                if keyword_check:        
-                    self.make_graph(keywords_list,self.thermo_keywords,self.thermo_data)
+                if keyword_check:
+                    self.make_graph(keywords_list, self.thermo_keywords,
+                                    self.thermo_data)
             elif graph == 'exit':
                 exit
             else:
-                print ('Invalid selection, try again')
+                print('Invalid selection, try again')
         return

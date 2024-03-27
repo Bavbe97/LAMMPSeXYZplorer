e6e45a599b15e62ecfa271a846b4e4b84ceb0681
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-27 10:35:24 +0100

    Attributes thermo_keyword and thermo_data are now thermo_df

--- lammpshade/GraphMaker.py
+++ lammpshade/GraphMaker.py
@@ -11,14 +11,11 @@ from lammpshade.Constructor import *
 
 class GraphMaker:
     def __init__(self, thermo_data, thermo_keywords, YAMLReader):
-        self.thermo_data = thermo_data
-        self.thermo_keywords = thermo_keywords
         self.file = YAMLReader
+        self.thermo_df = pd.Dataframe(thermo_data, columns=thermo_keywords)
         pass
 
     def make_graph(self): # pragma: no cover
-        thermo_df = pd.DataFrame(self.thermo_data,
-                                 columns=self.thermo_keywords)
         #color = []
         #markerstyle = []
         #linestyle = []
@@ -26,10 +23,10 @@ class GraphMaker:
         #grid = False
         gridstyle = '--'
         xlabel = 'Time ' + self.file.get_units('Time')
-        for column in thermo_df:
+        for column in self.thermo_df:
             if column not in ('Time', 'Step'):
                 fig, ax = plt.subplots()
-                ax.plot(thermo_df['Time'], thermo_df[column])
+                ax.plot(self.thermo_df['Time'], self.thermo_df[column])
                 ax.set_xlabel(xlabel)
                 y = column.replace('c_', '').replace('v_', '')
                 ylabel = y + ' ' + self.file.get_units(y.split('_')[0])
@@ -39,13 +36,16 @@ class GraphMaker:
         plt.show()
 
     def interact_graph(self): # pragma: no cover
-        print('Define graphs printing settings:')
         print('The following quantities have been found: ' +
               str(self.thermo_keywords)[1:-1])
         while True:
             keyword_check = True
+            print('Define printing settings:')
             print('Input format: mode [q_name1, q_name2, q_name3] \n' +
                   'To exit the program type: "exit"')
+            print('Modes\n')
+            print('Display - Show all the [Time vs. Quantity] graphs separately\n')
+            print('Stack - Shows a single [Time vs. Quantities] graph\n')
             graph = input('Select which quantities to display and how:\n')
             prova = graph.replace(' ', '').split('[')
             if prova[0] == 'display':

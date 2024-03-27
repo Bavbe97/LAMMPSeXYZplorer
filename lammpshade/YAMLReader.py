0f6cd4f88ae481f2c62635bded505f2126d39030
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-03-27 10:50:35 +0100

    Units dictionary is now a variable outside of the class

--- lammpshade/YAMLReader.py
+++ lammpshade/YAMLReader.py
@@ -5,7 +5,39 @@ Created on Wed Mar  6 12:28:31 2024
 @author: fbarb
 """
 
-
+units_mappings = {
+            'real': {
+                    'mass': r'(g/mol)',
+                    'distance': r'($/AA$)',
+                    'time': r'(fs)',
+                    'energy': r'$(kcal/mol)$',
+                    'velocity': r'($\AA$ / fs)',
+                    'force': r'$((kcal/mol)/\AA)$',
+                    'torque': r'$(kcal/mol)$',
+                    'temperature': r'(K)',
+                    'pressure': r'(atm)',
+                    'dynamic viscosity': r'(P)',
+                    'charge': r'm. of e. c.',
+                    'dipole': r'(charge \times \AA)$',
+                    'electric field': r'$(V/\AA)$',
+                    'density': r'($g/cm^\text{dim}$)'
+            },
+            'metal': {
+                    'mass': r'$\frac{\text{g}}{\text{mol}}$',
+                    'distance': r'\AA',
+                    'time': r'ps',
+                    'energy': r'eV',
+                    'velocity': r'$\frac{\text{\AA}}{\text{ps}}$',
+                    'force': r'$\frac{\text{eV}}{\text{\AA}}$',
+                    'torque': r'eV',
+                    'temperature': r'K',
+                    'pressure': r'bar',
+                    'dynamic viscosity': r'P',
+                    'charge': r'multiple of electron charge (1.0 is a proton)',
+                    'dipole': r'$\text{charge}\times\text{\AA}$',
+                    'electric field': r'V/\text{\AA}',
+                    'density': r'$\frac{\text{g}}{\text{cm}^\text{dim}}$'
+            }}
 class YAMLReader:
     def __init__(self, filename = None):
         self.filename = filename
@@ -140,41 +172,6 @@ class YAMLReader:
     
         # Obtain the value of 'units' attribute
         units = self.current_step['units']
-    
-        # Map units to their corresponding unit format
-        units_mappings = {
-            'real': {
-                    'mass': r'(g/mol)',
-                    'distance': r'($/AA$)',
-                    'time': r'(fs)',
-                    'energy': r'$(kcal/mol)$',
-                    'velocity': r'($\AA$ / fs)',
-                    'force': r'$((kcal/mol)/\AA)$',
-                    'torque': r'$(kcal/mol)$',
-                    'temperature': r'(K)',
-                    'pressure': r'(atm)',
-                    'dynamic viscosity': r'(P)',
-                    'charge': r'm. of e. c.',
-                    'dipole': r'(charge \times \AA)$',
-                    'electric field': r'$(V/\AA)$',
-                    'density': r'($g/cm^\text{dim}$)'
-            },
-            'metal': {
-                    'mass': r'$\frac{\text{g}}{\text{mol}}$',
-                    'distance': r'\AA',
-                    'time': r'ps',
-                    'energy': r'eV',
-                    'velocity': r'$\frac{\text{\AA}}{\text{ps}}$',
-                    'force': r'$\frac{\text{eV}}{\text{\AA}}$',
-                    'torque': r'eV',
-                    'temperature': r'K',
-                    'pressure': r'bar',
-                    'dynamic viscosity': r'P',
-                    'charge': r'multiple of electron charge (1.0 is a proton)',
-                    'dipole': r'$\text{charge}\times\text{\AA}$',
-                    'electric field': r'V/\text{\AA}',
-                    'density': r'$\frac{\text{g}}{\text{cm}^\text{dim}}$'
-            }}
 
         # Check if the 'units' value exists in the mappings
         if units in units_mappings:

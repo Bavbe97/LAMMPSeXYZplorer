f1b1ee30a5a6bd2eea528c57d33d8084d0a7a452
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-04-05 16:26:21 +0200

    Now XYZWriter can be used with 'with' statement. Constructor modified accordingly

--- lammpshade/Constructor.py
+++ lammpshade/Constructor.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  6 13:53:03 2024
-
-@author: fbarb
-"""
 from lammpshade.YAMLReader import *
 from lammpshade.XYZWriter import *
 from lammpshade.GraphMaker import *
@@ -11,8 +5,8 @@ import pandas as pd
 
 
 class Simulation:
-    def __init__(self, filename):
-        self.file = YAMLReader(filename)
+    def __init__(self, filepath):
+        self.file = YAMLReader(filepath)
         self.thermo_keywords = None
         self.thermo_data = None
         self.output = None
@@ -21,21 +15,21 @@ class Simulation:
     def convert_to_xyz(self, output):
         i = 0
         self.output = XYZWriter(output)
-        while True:
-            step = self.file.get_next_step()
-            if not step:
-                break
-            else:
-                if self.thermo_data is None:
-                    self.thermo_data = []
-                    self.thermo_keywords = step['thermo']['keywords']
-                    self.thermo_data.append(step['thermo']['data'])
+        with self.output as out:
+            while True:
+                step = self.file.get_next_step()
+                if not step:
+                    break
                 else:
-                    self.thermo_data.append(step['thermo']['data'])
-                self.output.write_to_xyz(step)
-                print('Step n. ', i, ' processed')
-                i += 1
-        self.output.close_file()
+                    if self.thermo_data is None:
+                        self.thermo_data = []
+                        self.thermo_keywords = step['thermo']['keywords']
+                        self.thermo_data.append(step['thermo']['data'])
+                    else:
+                        self.thermo_data.append(step['thermo']['data'])
+                    out.write_to_xyz(step)
+                    print('Step n. ', i, ' processed')
+                    i += 1
 
     def get_thermodata(self):
         i = 0

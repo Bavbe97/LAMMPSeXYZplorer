537b740ea55bebe204ca241e9f400f02ddf221b5
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-04-12 17:14:12 +0200

    Docs: added explanatory comments to main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 21 16:18:27 2024
-
-@author: fbarb
-"""
-
 import lammpshade as ls
 
-sim = ls.Simulation('./examples/input_example.yaml')
-sim.convert_to_xyz('./examples/xyz_example.xyz')
+# Example of how to use the package
+
+# The input file is a yaml file that contains the simulation parameters
+input = './examples/input_example.yaml'
+
+# The output file is the name of the file that will be created
+output = './examples/xyz_example.xyz'
+
+# The mode can be 'interactive' or 'combine'
+mode = 'interactive'
+
+# Create a simulation object
+sim = ls.Simulation(input)
+
+# Convert the simulation data to xyz format
+sim.convert_to_xyz(output)
+
+# Get the thermo data from the simulation
 thermo = sim.get_thermodata()
-sim.make_graphs(mode = 'interactive')
+
+# Create graphs based on the thermo data in an interactive mode
+sim.make_graphs(mode)

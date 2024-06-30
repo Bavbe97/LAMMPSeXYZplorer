a2a4d3c1e127c270c087f88d850fdf8f70b7f517
Francesco Barberini <francesco.barberini5@studio.unibo.it>
2024-06-30 18:42:56 +0200

    Added get_step_thermodata method to Constructor class. Modified comments to the methods to reflect changes and canon formatting.

--- lammpshade/Constructor.py
+++ lammpshade/Constructor.py
@@ -18,16 +18,26 @@ class Simulation:
     A class for processing LAMMPS simulation data.
 
     Attributes:
-    - file: The YAMLReader object for reading the simulation data file.
-    - thermo_keywords: The list of thermo keywords.
-    - thermo_data: The list of thermo data.
-    - graphs: The GraphMaker object for creating graphs.
+    file : YAMLReader
+        The YAMLReader object for reading the simulation data file.
+    thermo_keywords : list
+        The list of thermo keywords.
+    thermo_data : list
+        The list of thermo data.
+    graphs : GraphMaker
+        The GraphMaker object for creating graphs.
 
     Methods:
-    - __init__(self, filepath): Initializes the Simulation object.
-    - convert_to_xyz(self, output): Converts the simulation data to XYZ format.
-    - get_thermodata(self): Retrieves the thermo data from the simulation data.
-    - make_graphs(self, interact=False): Creates graphs from the thermo data.
+    __init__(self, filepath)
+        Initializes the Simulation object.
+    convert_to_xyz(self, output)
+        Converts the simulation data to XYZ format.
+    get_thermodata(self)
+        Retrieves the thermo data from the simulation data.
+    get_step_thermodata(self, step)
+        Retrieves the thermo data from the simulation step data.
+    make_graphs(self, interact=False)
+        Creates graphs from the thermo data.
 
     """
 
@@ -43,16 +53,20 @@ class Simulation:
         self.thermo_keywords = None
         self.thermo_data = None
 
-    def convert_to_xyz(self, output):
+    def convert_to_xyz(self, output, thermo_flag=True):
         """
         Converts the simulation data to XYZ format.
 
         Parameters:
         - output: The path to the output XYZ file.
+        - thermo_flag: A boolean indicating if thermo data should be included.
+            Default is True.
+        
+        Returns:
+        - None
 
         """
         i = 0
-        thermo_flag = True
         self.output = XYZWriter(output)
         with self.output as out:
             while True:
@@ -60,18 +74,7 @@ class Simulation:
                 if not step:
                     break
                 if thermo_flag:
-                    if self.thermo_keywords is None:
-                        try:
-                            self.thermo_keywords = step['thermo']['keywords']
-                            self.thermo_data = []
-                            self.thermo_data.append(step['thermo']['data'])
-                        except KeyError:
-                            print('No thermo data found in the file')
-                            thermo_flag = False
-                        finally:
-                            pass
-                    else:
-                        self.thermo_data.append(step['thermo']['data'])
+                    thermo_flag = self.get_step_thermodata(step)
                 out.write_to_xyz(step)
                 print('Step n. ', i, ' processed')
                 i += 1
@@ -87,34 +90,67 @@ class Simulation:
         i = 0
         thermo_flag = True
         if self.thermo_data is None:
-            self.thermo_data = []
+            step = self.file.get_next_step()
             while True:
-                step = self.file.get_next_step()
                 if not step:
                     break
-                if thermo_flag:
-                    if self.thermo_keywords is None:
-                        try:
-                            self.thermo_keywords = step['thermo']['keywords']
-                        except KeyError:
-                            print('No thermo data found in the file')
-                            thermo_flag = False
-                            return None
-                        else:
-                            self.thermo_data.append(step['thermo']['data'])
-                print('Step n. ', i, ' processed')
-                i += 1
-            thermo = pd.DataFrame(self.thermo_data,
-                                  columns=self.thermo_keywords)
-            if (thermo == 0).all().all():
-                print('No thermo data found in the file')
-                return None
-            else:
+
+                elif thermo_flag:
+                    thermo_flag = self.get_step_thermodata(step)
+                    print('Step n. ', i, ' processed')
+                    i += 1
+                    step = self.file.get_next_step()
+
+                elif not thermo_flag:
+                    print('No thermo data found in the file')
+                    break
+
+            if thermo_flag:
+                thermo = pd.DataFrame(self.thermo_data,
+                                      columns=self.thermo_keywords)
                 return thermo
+            else:
+                return None
         else:
             thermo = pd.DataFrame(self.thermo_data,
                                   columns=self.thermo_keywords)
             return thermo
+
+    def get_step_thermodata(self, step):
+        """
+        Retrieves the thermo data from the simulation step data.
+        Append the thermo data to the thermo_data attribute.
+
+        Parameters:
+        - step: The simulation step data.
+
+        Returns:
+        - thermo_flag: A boolean indicating if thermo data was found.
+
+        Raises:
+        - Exception: If no thermo data is found in the step.
+        """
+        thermo_flag = True
+        if self.thermo_keywords is None:
+            # Attempt to get thermo data from the step
+            try:
+                self.thermo_keywords = []
+                self.thermo_keywords = step['thermo']['keywords']
+            except KeyError('No thermo data found in the step'):
+                # No thermo data found in the step
+                print('No thermo data found in the file')
+                thermo_flag = False
+                # Return thermo_flag to indicate no thermo data found
+                return thermo_flag
+            else:
+                # Append thermo data to the thermo_data attribute
+                self.thermo_data = []
+                self.thermo_data.append(step['thermo']['data'])
+                return thermo_flag
+        else:
+            # Append thermo data to the thermo_data attribute
+            self.thermo_data.append(step['thermo']['data'])
+            return thermo_flag
 
     def make_graphs(self, mode='display'):
         """
@@ -132,9 +168,15 @@ class Simulation:
             ValueError: If an invalid mode is provided.
                 Valid values are "display" and "interactive".
         """
+        # Get thermo data
         thermo_df = self.get_thermodata()
+        # Create GraphMaker object
         self.graphs = GraphMaker(thermo_df)
+
+        # Generate graphs
         if not mode.startswith('d') and not mode.startswith('i'):
+            # Invalid mode
             raise ValueError('Invalid mode. '
                              'Valid values are "display" and "interactive"')
-        self.graphs.run(mode)
+        else:
+            self.graphs.run(mode)
